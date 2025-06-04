bee1910f79acc0980cd75bc0329445a098aa519d
ab <andre.berlemont@gmail.com>
2025-06-04 19:23:52 +0200

    auto :autofill

--- Runtime/importer/LocaDataSheet.cs
+++ Runtime/importer/LocaDataSheet.cs
@@ -5,63 +5,77 @@ using UnityEngine;
 
 namespace fwp.localizator
 {
-    
-    /// <summary>
-    /// base structure to get a scriptble with uid/tabid couples
-    /// - url
-    /// - tabs @ spreadsheet
-    /// </summary>
 
-    [CreateAssetMenu(menuName = LocalizationManager._asset_menu_path + "create sheet", order = LocalizationManager._asset_menu_order)]
-    public class LocaDataSheet : ScriptableObject
-    {
-        // https://docs.google.com/spreadsheets/d/[my-uid]/edit#gid=[tab-uid]
+	/// <summary>
+	/// base structure to get a scriptble with uid/tabid couples
+	/// - url
+	/// - tabs @ spreadsheet
+	/// </summary>
 
-        public const string googleSpreadsheetBaseUrl = "https://docs.google.com/spreadsheets/d/";
+	[CreateAssetMenu(menuName = LocalizationManager._asset_menu_path + "create sheet", order = LocalizationManager._asset_menu_order)]
+	public class LocaDataSheet : ScriptableObject
+	{
+		// https://docs.google.com/spreadsheets/d/[my-uid]/edit#gid=[tab-uid]
 
-        [Tooltip("string to identify spreadsheet")]
-        public string sheetUrlUid;
+		public const string googleSpreadsheetBaseUrl = "https://docs.google.com/spreadsheets/d/";
 
-        [Tooltip("to be able to identify what spreadsheet it is")]
-        public string sheetDescription;
+		[Tooltip("string to identify spreadsheet")]
+		public string sheetUrlUid;
 
-        [Tooltip("all tabs in that spreadsheet")]
-        public DataSheetTab[] tabs;
+		[Tooltip("to be able to identify what spreadsheet it is")]
+		public string sheetDescription;
 
-        public string url => googleSpreadsheetBaseUrl + sheetUrlUid;
+		[Tooltip("all tabs in that spreadsheet")]
+		public DataSheetTab[] tabs;
 
-        public int getTabIndex(DataSheetTab tab)
-        {
-            for (int i = 0; i < tabs.Length; i++)
-            {
-                if (tabs[i].compare(tab))
-                    return i;
-            }
-            return -1;
-        }
+		public string url => googleSpreadsheetBaseUrl + sheetUrlUid;
 
-        private void OnValidate()
-        {
-            if (Application.isPlaying)
-                return;
+#if UNITY_EDITOR
+		[ContextMenu("* parse type : autofill")]
+		void cmParseType()
+		{
+			for (int i = 0; i < tabs.Length; i++)
+			{
+				var t = tabs[i];
+				t.parseType = SheetParseType.autofill;
+				tabs[i] = t;
+			}
+			UnityEditor.EditorUtility.SetDirty(this);
+		}
+#endif
 
-            if (sheetUrlUid.StartsWith(GoogleSpreadsheetBridge.sheetUrlPrefix))
-            {
-                sheetUrlUid = sheetUrlUid.Replace(GoogleSpreadsheetBridge.sheetUrlPrefix, string.Empty);
-            }
-        }
+		public int getTabIndex(DataSheetTab tab)
+		{
+			for (int i = 0; i < tabs.Length; i++)
+			{
+				if (tabs[i].compare(tab))
+					return i;
+			}
+			return -1;
+		}
 
-        public string getMatchingLabel(string tabId) => getTabIdOfField(tabId).ToString().ToLower();
+		private void OnValidate()
+		{
+			if (Application.isPlaying)
+				return;
 
-        public string getTabIdOfField(string field)
-        {
-            for (int i = 0; i < tabs.Length; i++)
-            {
-                if (tabs[i].tabName == field) return tabs[i].tabUrlId;
-            }
-            return string.Empty;
-        }
+			if (sheetUrlUid.StartsWith(GoogleSpreadsheetBridge.sheetUrlPrefix))
+			{
+				sheetUrlUid = sheetUrlUid.Replace(GoogleSpreadsheetBridge.sheetUrlPrefix, string.Empty);
+			}
+		}
 
-    }
+		public string getMatchingLabel(string tabId) => getTabIdOfField(tabId).ToString().ToLower();
+
+		public string getTabIdOfField(string field)
+		{
+			for (int i = 0; i < tabs.Length; i++)
+			{
+				if (tabs[i].tabName == field) return tabs[i].tabUrlId;
+			}
+			return string.Empty;
+		}
+
+	}
 
 }
